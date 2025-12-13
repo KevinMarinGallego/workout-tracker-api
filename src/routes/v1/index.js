65ec203af3e74f720c7524939a58abc051634513
kevin marin <kevinmg253@gmail.com>
2025-12-13 11:50:16 -0500

    se acutilza en index

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -5,10 +5,12 @@ const router = express.Router();
 // IMPORTA correctamente el router de usuarios
 const usersRoutes = require('./users.routes'); // <-- esto faltaba
 const exercisesRoutes = require('./exercises.routes');
+const workoutsRoutes = require('./workouts.routes');
 
 
 
 router.use('/users', usersRoutes);
 router.use('/exercises', exercisesRoutes);
+router.use('/workouts', workoutsRoutes);
 
 module.exports = router;
